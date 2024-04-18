b550f6805c49e78e0ec7de910e79dce61309c6a8
Zach Hartzog <zlhartzog@gmail.com>
2024-04-18 14:38:49 -0400

    feat: add auth setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { PMHeader, Youths} from './ui-components'
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import config from "./amplifyconfiguration.json";
+import { PMHeader, Youths } from "./ui-components";
 
-function App() {
+Amplify.configure(config);
+
+function App({ signOut, user }) {
   return (
-   <>
-    <PMHeader width="100%" />
-    <Youths/>
-   </>
+    <>
+      <h1>Hello {user.username}</h1>
+      <button onClick={signOut}>Sign out</button>
+      <PMHeader width="100%" />
+      <Youths />
+    </>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
