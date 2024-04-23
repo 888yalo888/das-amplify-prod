dbf53f5968cff69e5cee4ac4a22dccae7a12ec47
Patrick Tran <patricktran048@gmail.com>
2024-04-23 13:24:41 -0500

    default to site selection after login

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
+  useNavigate,
 } from "react-router-dom";
 import CheckIn from "./pages/CheckIn";
 import CheckOut from "./pages/CheckOut";
@@ -13,6 +13,7 @@ import Roster from "./pages/Roster";
 import "@aws-amplify/ui-react/styles.css";
 import "./App.css";
 import { Amplify } from "aws-amplify";
+import { Hub } from 'aws-amplify/utils';
 import config from "./amplifyconfiguration.json";
 import CheckedOutDetails from "./pages/AttendanceDetails";
 import DASNAuthenticator from "./pages/DASNAuthenticator";
@@ -26,29 +27,36 @@ const App = () => {
   const { signOut, user, authStatus } = useAuthenticator(
     ({ signOut, user, authStatus }) => [signOut, user, authStatus]
   );
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    Hub.listen('auth', (data) => {
+      if (data?.payload?.event === 'signedIn') {
+        navigate("/site-selection");
+      }
+    });
+  }, []);
 
   return (
     <DASNAuthenticator>
       <button onClick={signOut}>Sign out</button>
-      <Router>
-        <Header></Header>
-        <Routes>
-          <Route path="/" element={<Navigate replace to="/site-selection" />} />
-          <Route path="/check-in" element={<CheckIn />} />
-          <Route path="/check-out/:youthID" element={<CheckOut />} />
-          <Route
-            path="/attendance-details/:youthID"
-            element={<CheckedOutDetails />}
-          />
-          <Route path="/roster" element={<Roster />} />
-          <Route path="/vibe-check/:youthID" element={<VibeCheck />} />
-          <Route path="/youth-details/:youthID" element={<YouthDetails />} />
-          <Route
-            path="/site-selection"
-            element={<SiteSelection user={user} />}
-          />
-        </Routes>
-      </Router>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Navigate replace to="/site-selection" />} />
+        <Route path="/check-in" element={<CheckIn />} />
+        <Route path="/check-out/:youthID" element={<CheckOut />} />
+        <Route
+          path="/attendance-details/:youthID"
+          element={<CheckedOutDetails />}
+        />
+        <Route path="/roster" element={<Roster />} />
+        <Route path="/vibe-check/:youthID" element={<VibeCheck />} />
+        <Route path="/youth-details/:youthID" element={<YouthDetails />} />
+        <Route
+          path="/site-selection"
+          element={<SiteSelection user={user} />}
+        />
+      </Routes>
     </DASNAuthenticator>
   );
 };
