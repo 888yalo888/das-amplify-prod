2f78cd410717425ba36647906eede6036ab95a85
jprejean-pariveda <v561957@amerisourcebergen.com>
2024-04-25 08:10:40 -0500

    Added flexible styling

--- src/pages/VibeCheck.js
+++ src/pages/VibeCheck.js
@@ -3,16 +3,22 @@ import { ButtonCheckInVibe, CheckInVibe } from "../ui-components";
 
 import { useNavigate, useParams } from "react-router-dom";
 import { checkInYouth, getYouthInfo } from "../services/api.service";
-import { Vibe } from '../enums/vibe.enum';
+import { Vibe } from "../enums/vibe.enum";
 import useStore from "../store/store";
+import EmoteAngry from "../assets/EmoteAngry.png";
+import EmoteAtEase from "../assets/EmoteAtEase.png";
+import EmoteHappy from "../assets/EmoteHappy.png";
+import EmoteSad from "../assets/EmoteSad.png";
+import { useMediaQuery } from "react-responsive";
+
 
 const VibeCheck = () => {
   const site = useStore((state) => state.currentSite);
   const navigate = useNavigate();
   const { youthID } = useParams();
-
+  
   const [youth, setYouth] = React.useState();
-
+  
   React.useEffect(() => {
     const fetchYouthData = async () => {
       setYouth(await getYouthInfo(youthID));
@@ -24,58 +30,80 @@ const VibeCheck = () => {
       setYouth(foundYouthData);
     }
   }, []);
-
-  const [selectedVibe, setSelectedVibe] = React.useState();  
+  
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 760px)" });
+  const [selectedVibe, setSelectedVibe] = React.useState();
 
   const VibeOptions = () => {
     function onOptionClick(vibe) {
       setSelectedVibe(vibe);
     }
-    
+
     function getOverrides(vibe) {
       return {
         CheckInVibe: {
-          className: `check-in-option ${selectedVibe === vibe ? 'check-in-option__selected' : ''}`,
+          className: `check-in-option ${
+            selectedVibe === vibe ? "check-in-option__selected" : ""
+          }`,
           onClick: () => onOptionClick(vibe),
         },
-        'At Ease': {
+        Content: vibe,
+        Emote: {
+          src: vibe === Vibe.Angry ? EmoteAngry
+          : vibe === Vibe.AtEase ? EmoteAtEase
+          : vibe === Vibe.Sad ? EmoteSad
+          : vibe === Vibe.Happy ? EmoteHappy
+          : null,
+          alt: vibe,
+        },
+        Label: {
           children: vibe,
         },
       };
     }
-    const vibes = Object.values(Vibe).map((vibe) => <CheckInVibe key={vibe} overrides={getOverrides(vibe)} />);
-    
+    const vibes = Object.values(Vibe).map((vibe) => <CheckInVibe key={vibe} overrides={getOverrides(vibe)}/>);
+
     return vibes;
-  }
+  };
 
   async function onCheckInClick() {
-    await checkInYouth(site.id, youth.id, selectedVibe).catch((error) => console.log('check in error', error));
-    navigate('/check-in');
+    await checkInYouth(site.id, youth.id, selectedVibe).catch((error) =>
+      console.log("check in error", error)
+    );
+    navigate("/check-in");
   }
 
   const checkInButtonOverrides = {
     ButtonCheckInVibe: {
-      className: 'check-in-btn',
+      className: "check-in-btn",
     },
     ButtonCheckInVibe6151869: {
-      isDisabled: !selectedVibe
+      isDisabled: !selectedVibe,
+      margin: "auto",
+      width: isTabletOrMobile ? "350px": "400px"
     },
   };
 
+
+
   return (
     <div
-    style={{
-      display: "flex",
-      flexDirection: 'column',
-      justifyContent: "center",
-      alignItems: 'center',
-      margin: "10px 20px",
-    }}
-  >
-    <h1>Hey {youth?.fullName.split(' ')[0]}, what's your vibe today?</h1>
-    <VibeOptions></VibeOptions>
-    <ButtonCheckInVibe overrides={checkInButtonOverrides} onClick={onCheckInClick}/>
-  </div>
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "10px 20px",
+        textAlign: "center",
+      }}
+    >
+      <h1>Hey {youth?.fullName.split(" ")[0]}, what's your vibe today?</h1>
+      <VibeOptions>Angry</VibeOptions>
+      <ButtonCheckInVibe 
+        overrides={checkInButtonOverrides}
+        onClick={onCheckInClick}
+      />
+    </div>
   );
 };
 
